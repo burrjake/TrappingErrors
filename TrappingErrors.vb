71569e836d9867d658805e0f036080c320a59978
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-28 08:32:53 -0600

    add catch exception

--- TrappingErrors.vb
+++ TrappingErrors.vb
@@ -1,7 +1,19 @@
 ﻿Module TrappingErrors
 
     Sub Main()
+        Dim userInput As String
+        Dim userNumber As Integer
 
+        Try
+            Console.WriteLine("Please type a number and press enter")
+            userInput = Console.ReadLine()
+            userNumber = CInt(userInput)
+        Catch ex As InvalidCastException
+
+            Console.WriteLine(userInput & " + 5 is: ")
+            Console.WriteLine(userNumber + 5)
+            Console.ReadLine()
+        End Try
     End Sub
 
 End Module
