1e272d7e238b02dfdf511fb49a063cfdfe77b9c9
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-28 08:56:09 -0600

    add loop

--- TrappingErrors.vb
+++ TrappingErrors.vb
@@ -1,19 +1,29 @@
-﻿Module TrappingErrors
+﻿Imports System.Linq.Expressions
+
+Module TrappingErrors
 
     Sub Main()
         Dim userInput As String
         Dim userNumber As Integer
 
-        Try
-            Console.WriteLine("Please type a number and press enter")
-            userInput = Console.ReadLine()
-            userNumber = CInt(userInput)
-        Catch ex As InvalidCastException
+        Do
+            Try
+                Console.WriteLine("Please type a number and press enter. Enter Q to quit.")
+                userInput = Console.ReadLine()
+                userNumber = CInt(userInput)
+            Catch ex As InvalidCastException
+                Console.WriteLine("I asked for a number you entered: " & userInput)
+            Catch ex As OverflowException
+                Console.WriteLine("Sorry, the number you entered is too big")
 
-            Console.WriteLine(userInput & " + 5 is: ")
-            Console.WriteLine(userNumber + 5)
-            Console.ReadLine()
-        End Try
+                Console.WriteLine(userInput & " + 5 is: ")
+                Console.WriteLine(userNumber + 5)
+                Console.ReadLine()
+            Catch ex As Exception
+                Console.WriteLine("You broke it")
+                Console.WriteLine(ex.Message)
+            End Try
+        Loop Until userInput = "Q" Or userInput = "q"
     End Sub
 
 End Module
